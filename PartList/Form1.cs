b558b51c89177bd63a1d291c3054046489c9495f
Gabriel Marussi <gabrielmesquitacwb@gmail.com>
2022-06-07 17:24:12 -0300

    Adicionar arquivos de projeto.

---
+++ PartList/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace PartList
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            SpreadsheetCreator.CreateTable();
+            Process.Start(new ProcessStartInfo(@"C:\temp\testeExcel.xlsx"));
+        }
+    }
+}
---
+++ PartList/SpreadsheetCreator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ClosedXML.Excel;
+
+namespace PartList
+{
+
+    class SpreadsheetCreator
+    {
+        
+        static public void CreateTable()
+        {
+            var wb = new XLWorkbook();
+
+            var ws = wb.Worksheets.Add("Plan1");
+
+            ws.Cell("A1").Value = "TESTE";
+            ws.Cell("B1").Value = "TEST";
+            ws.Cell("C1").Value = "TESE";
+            ws.Cell("D1").Value = "TETE";
+            ws.Cell("E1").Value = "TSTE";
+
+            wb.SaveAs(@"C:\temp\testeExcel.xlsx");
+        }
+    }
+}
